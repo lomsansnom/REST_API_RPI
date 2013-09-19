2666e01614353fda32e66917df82aea6e8b6beb7
alex <alex@localhost.localdomain>
2013-09-19 14:11:36 +0200

    debug

--- restServer.py
+++ restServer.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
                     'server.socket_port': 8282,
                     },
          '/': {
-               'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+               'request.dispatch': cherrypy.dispatch.MethodDispatcher()
                }
         })
 
