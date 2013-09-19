de65c584ddb06a2ce239df365cdb164a5a52571d
alex <alex@localhost.localdomain>
2013-09-19 14:47:58 +0200

    test

--- restServer.py
+++ restServer.py
@@ -5,26 +5,20 @@ from cherrypy import expose
 import simplejson
 
 class restRPI:
-    @expose
-    def GET(self):
-        return "It's working"
     
-    @expose
-    def POST(self, json):
-        dict = simplejson.loads(json)
-        return 'ok'
+    def __init__(self):
+        cherrypy.response.headers['Content-Type'] = "application/json"
+        
+    def test(self):
+        ret={"year":"test"}
+        ret['month']='nothing'
+        return simplejson.dumps(ret)
+    
+conf={
+        'global':{
+                  'server.socket_host' : 0.0.0.0,
+                  'server.socket_port' : 8282
+        }
+}
 
-if __name__ == '__main__':
-
-    cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                            'server.socket_port': 8282,
-                       })
-    cherrypy.tree.mount(
-        restRPI(), '/api',
-        {
-         '/':       
-                    {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-        })
-
-    cherrypy.engine.start()
-    cherrypy.engine.block()   
+cherrypy.quickstart(resrRPI(),"/", conf)
