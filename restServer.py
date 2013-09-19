482c6306c1ad2120dd670b642b5b61945ccf2113
alex <alex@localhost.localdomain>
2013-09-19 14:09:29 +0200

    test

--- restServer.py
+++ restServer.py
@@ -13,16 +13,20 @@ class restRPI:
     def POST(self, json):
         dict = simplejson.loads(json)
         return dict
-        
-conf = {
-    'global': {
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': 8282,
-    },
-    '/': {
-        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-    }
-}
 
-cherrypy.quickstart(restRPI(), '/', conf)
+if __name__ == '__main__':
 
+    cherrypy.tree.mount(
+        restRPI(), '/',
+        {
+         'global': {
+                    'server.socket_host': '0.0.0.0',
+                    'server.socket_port': 8282,
+                    },
+         '/': {
+               'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+               }
+        })
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()   
