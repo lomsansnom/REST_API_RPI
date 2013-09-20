c96aa6f6e96a8161371b96de0f34316b94f5aa9f
alex <alex@localhost.localdomain>
2013-09-20 19:34:17 +0200

    debug

--- restServer.py
+++ restServer.py
@@ -58,16 +58,16 @@ class restRPI:
             elif params['query'] == 'ajouterMembre':
                 requete = """INSERT INTO "Utilisateurs" ("login", "password") VALUES (\''""" + params['username'] + """'\', \''""" + params['password'] + """'\')""";
 
-            try:
-                sessionDB = psycopg2.connect(host = self.__host, port = self.__port, dbname = self.__dbname, user = self.__user, password = self.__password)
-                curseur = sessionDB.cursor()
-                curseur.execute(requete)
-                cherrypy.log(','.join(map(str, curseur.fetchall())))
-                ret = {'OK' : True}
-            except:
-                cherrypy.log("Erreur lors de la connexion a la DB")
-                ret = {"OK" : False}
-                ret['Erreur'] = "Erreur lors de la connexion a la DB"
+            #try:
+            sessionDB = psycopg2.connect(host = self.__host, port = self.__port, dbname = self.__dbname, user = self.__user, password = self.__password)
+            curseur = sessionDB.cursor()
+            curseur.execute(requete)
+            cherrypy.log(','.join(map(str, curseur.fetchall())))
+            ret = {'OK' : True}
+#            except:
+ #               cherrypy.log("Erreur lors de la connexion a la DB")
+  #              ret = {"OK" : False}
+   #             ret['Erreur'] = "Erreur lors de la connexion a la DB"
         else:
             ret = {"OK" : False}
             ret['Erreur'] = "query, username et password sont obligatoires"
