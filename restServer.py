c526574d251b2cfd8603a167648b802e27f29db7
alex <alex@localhost.localdomain>
2013-09-23 19:42:57 +0200

    debug

--- restServer.py
+++ restServer.py
@@ -59,8 +59,8 @@ class restRPI:
         
         if 'query' and 'username' and 'password' in params:
             if params['query'] == 'login':
-                requete = """SELECT "password" FROM "Utilisateurs" WHERE "login" = %(user)s;"""
-                donnees = {'user' : str(params['username'])}
+                requete = """SELECT "password" FROM "Utilisateurs" WHERE "login" = %s;"""
+                donnees = (tuple(params['username']),)
                 output = True
             elif params['query'] == 'ajouterMembre':
                 requete = """INSERT INTO "Utilisateurs" ("login", "password") VALUES (%s, %s);"""
