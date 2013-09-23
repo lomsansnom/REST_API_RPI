4c2de94901d506cdd6f87aa5e38fef085673505a
alex <alex@localhost.localdomain>
2013-09-23 19:16:35 +0200

    debug

--- restServer.py
+++ restServer.py
@@ -60,7 +60,7 @@ class restRPI:
         if 'query' and 'username' and 'password' in params:
             if params['query'] == 'login':
                 requete = """SELECT "password" FROM "Utilisateurs" WHERE "login" = %s;"""
-                donnees = (params['username'],)
+                donnees = ("'"+ params['username'] + "'",)
                 output = True
             elif params['query'] == 'ajouterMembre':
                 requete = """INSERT INTO "Utilisateurs" ("login", "password") VALUES (%s, %s);"""
@@ -75,8 +75,7 @@ class restRPI:
                 if not output:
                     sessionDB.autocommit = True
                     
-                #curseur = sessionDB.cursor(cursor_factory = psycopg2.extras.DictCursor)
-                curseur = sessionDB.cursor()
+                curseur = sessionDB.cursor(cursor_factory = psycopg2.extras.DictCursor)
                 curseur.execute(requete, donnees)
                 
                 if output:
